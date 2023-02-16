2d4b0b2e02b2bb23f77a83b86441a0ab0846a8e0
Tshegofatsonline1 <magometshegofatso7@gmail.com>
2023-02-16 11:01:34 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,12 @@
  */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
+
+	char a;
+        int b;
+        long int c;
+        long long int d;
+        float f;
 
 printf("Size of a char: %lu 1 byte(s)\n", (unsigned long)sizeof(a));
 prinf("Size of an int: %lu  4 byte(s)\n", (unsigned long)sizeof(b));
