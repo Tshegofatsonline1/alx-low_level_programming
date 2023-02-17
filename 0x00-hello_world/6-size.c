5a5d22469a7b0600f8dbd25d4624ac6f1e4fe84a
Tshegofatsonline1 <magometshegofatso7@gmail.com>
2023-02-16 23:51:37 -0800

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,9 @@
  * main -entry point
  * Description: 'the program's description
  * Return: always 0 success
- */
+ */ 
+
+
 int main(void)
 {
 char a;
@@ -13,10 +15,10 @@ long int c;
 long long int d;
 float f;
 
-printf("Size of a char: %d byte(s)\n", sizeof(char));
-printf("Size of an int: %d byte(s)\n", sizeof(int));
-printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-printf("Size of a float: %d byte(s)\n ", sizeof(float));
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of an int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+printf("Size of a float: %ld byte(s)\n ", sizeof(float));
 return (0);
 }
