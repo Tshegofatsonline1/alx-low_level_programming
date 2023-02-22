af5d56414134788a5dd12f05775b243d27210582
Tshegofatsonline1 <magometshegofatso7@gmail.com>
2023-02-22 08:00:24 -0800

    putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include<main.h>
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -7,7 +7,7 @@
  * Return: Always 0 (Success)
  */
 
-int main(void);
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,9 @@
+#include"main.h"
+#include<unistd.h>
+/**
+ * _putchar - writes the character C to stdout
+ * @c The character to print
+ *
+ * Return: on success 1.
+ * on error -1 is returned and errno is set appropriately
+ */
