89c6bb3025b5fd0fbe55f045a7d8a32431063a12
Tshegofatsonline1 <magometshegofatso7@gmail.com>
2023-02-22 00:08:29 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description:'program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void);
+{
+	printf("_putchar\n");
+	return (0);
+}
