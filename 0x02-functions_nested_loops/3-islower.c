2a730430370a78040ed83867070724ed9a5b02f5
Tshegofatsonline1 <magometshegofatso7@gmail.com>
2023-02-22 11:01:06 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _islower - Check main
+ * @c: input checked
+ * Description: program that checks for lowercase character
+ *
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	char j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		if (j == c)
+		m=1;
+	}
+
+	return (0);
+}
+
