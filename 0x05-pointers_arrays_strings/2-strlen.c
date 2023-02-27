582dc7a7e3afae2fd1738c4753fbe78e1bcb1c3b
Tshegofatsonline1 <magometshegofatso7@gmail.com>
2023-02-27 11:16:43 -0800

    3-puts

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,8 +2,8 @@
 
 /**
  * _strlen - Returns the length of a string.
- * @str: The sting to get the length of
- * Return: The length of @str.
+ * @str: The strin to get the length of
+ * Return: The length of @str
  */
 
 size_t _strlen(const char *str)
@@ -14,4 +14,3 @@ while (*str++)
 length++;
 return (length);
 }
-
