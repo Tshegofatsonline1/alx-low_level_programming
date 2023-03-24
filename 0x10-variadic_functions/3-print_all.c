6e802f627507a40f0133e9de8912a73e63db7fc3
Tshegofatsonline1 <magometshegofatso7@gmail.com>
2023-03-24 08:28:52 -0700

    3-print-all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,7 +12,6 @@ void print_all(const char * const format, ...)
 int i = 0;
 char *str, *sep = "";
 
-
 va_list list;
 
 
@@ -48,8 +47,6 @@ sep = ", ";
 i++;
 }
 }
-
-
 printf("\n");
 va_end(list);
 }
