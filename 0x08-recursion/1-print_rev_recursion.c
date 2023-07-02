732753487344cac23748912d3b0fd2a98927f3ef
Tshegofatsonline1 <magometshegofatso7@gmail.com>
2023-07-02 11:16:20 -0700

    rev recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _print_rev_recursion - a function that prints a string in reverse
@@ -9,18 +8,11 @@
  * return: (0);
  */
 
-void _print_rev_recursion(char *s);
-{
-char *s [] = ("Colton Walker");
-print_reverse(s);
-printf("/n");
-return 0;
-}
 void _print_rev_recursion(char *s)
 {
-if (*s != '/0');
+if (*s)
 {
-print_reverse(s + 1);
-printf("%c", *s);
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
 }
